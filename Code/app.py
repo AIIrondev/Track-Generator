1339aaaa1fc56d088396ee188f7315616a6ad9e4
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-08-04 21:45:42 +0200

    chore: Refactor app.py to use tkinter for GUI

--- Code/app.py
+++ Code/app.py
@@ -1,31 +1,15 @@
-import pygame as pg
-import sys
+import tkinter as tk
 
-
-# main function
 def main():
-    screen = pg.display.set_mode((800, 600))
-    pg.display.set_caption('Hello World')
-    clock = pg.time.Clock()
-    bg_color = pg.Color('gray12')
-    font = pg.font.Font(None, 64)
-    text = font.render('Hello World', True, pg.Color('dodgerblue'))
-    text_rect = text.get_rect(center=screen.get_rect().center)
-
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                return
-
-        screen.fill(bg_color)
-        screen.blit(text, text_rect)
-        pg.display.flip()
-        clock.tick(30)
+    root = tk.Tk()
+    root.title("Hello World")
+    root.geometry("800x600")
     
+    root.mainloop()
+
+def trackgerator():
+    pass
     
     
 if __name__ == '__main__':
-    pg.init()
     main()
-    pg.quit()
-    sys.exit()
