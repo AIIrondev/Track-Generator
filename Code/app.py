fc622bb4bd4d61b6c6bdb0bcb43b930e6b0e6e1b
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-08-05 21:09:00 +0200

    chore: Update app.py to use customtkinter for GUI

--- Code/app.py
+++ Code/app.py
@@ -1,14 +1,15 @@
-import tkinter as tk
+import customtkinter as ctk
 
 def main():
-    root = tk.Tk()
-    root.title("Hello World")
+    root = ctk.Tk()
+    root.title("Trackgerator")
     root.geometry("800x600")
-    
+    trackgerator()
     root.mainloop()
 
 def trackgerator():
-    pass
+    ctk.PhotoImage(file="images/trackgerator.png")
+    ctk.CtkLabel(text="Trackgerator", font=("Arial", 24)).pack()
     
     
 if __name__ == '__main__':
