c20bd48bb09b2e2930e9d9f909c2a14832028c1c
Maximilian Gründinger <iron.ai.dev@gmail.com>
2024-08-10 21:25:32 +0200

    Refactor button_callback function to use match statement for button actions

---
+++ Test.py
@@ -0,0 +1,17 @@
+class record:
+    def __init__(self):
+        # This will be the function that will be used to record the data
+        pass
+    
+    def up(self):
+        print("Upwards button clicked")
+        
+        
+    def down(self):
+        print("Downwards button clicked")
+        
+    def left(self):
+        print("Leftwards button clicked")
+        
+    def right(self):
+        print("Rightwards button clicked")
