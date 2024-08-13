61b6694105d9f4e1a8852cf3aa808c376fc14dc3
Maximilian Gründinger <iron.ai.dev@gmail.com>
2024-08-13 15:12:37 +0200

    Refactor save class to generate additional switch and generate_ab functions

--- Test_3.py
+++ Test_3.py
@@ -14,12 +14,25 @@ class save:
             for i in self.content:
                 match i:
                     case 1:
-                        f.write("    drive(10)")
+                        f.write("drive(10)")
                     case 2:
-                        f.write("    tank(180)")
+                        f.write("tank(180)")
                     case 3:
-                        f.write("    drive(-10)")
+                        f.write("drive(-10)")
                     case 4:
-                        f.write("    tank(-180)")
-            f.write("main.init()\nswitch()\ncalibrate()\nai.run({'Calibration': calibration, 'Akku': 85, 'Wheelusage': 0.95})")
-        pass
+                        f.write("tank(-180)")
+                    case 5:
+                        generate_ab_function = 0
+                        f.write(f"generate_ab({generate_ab_function})")
+                        generate_ab_function += 1
+            f.write("main.init()\nswitch()\ncalibrate()\nai.run({'Calibration': calibration, 'Akku': 85, 'Wheelusage': 0.95})\n")
+            for i in range(generate_ab_function):
+                f.write(f"switch()\n")
+                f.write(f"generate_ab({i})\n")
+            f.write("main.run()")
+
+
+if __name__ == "__main__":
+    save().convert_to_scsp()
+    shutil.move("Data/temp/temp.scsp", "Data/config/temp.scsp")
+    os.remove("Data/config/path.txt")
